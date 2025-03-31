1f95bca7852f3b74d04b79c6a186d5f1395505f6
theHTMLSpot <lagdenethan@gmail.com>
2025-03-31 05:38:10 +0200

    updated description blocks

--- components/description_block/page.tsx
+++ components/description_block/page.tsx
@@ -2,9 +2,9 @@ import { LucideIcon } from "lucide-react";
 
 export default function DescriptionBlock({ Icon, text }: { Icon: LucideIcon; text: string }) {
   return (
-    <div className="flex flex-col items-center text-center bg-white shadow-lg p-6 rounded-xl max-w-xs border border-gray-200">
+    <div className="grid place-items-center text-center bg-white shadow-lg p-6 rounded-xl max-w-xs border border-gray-200 aspect-square">
       {/* Icon */}
-      <Icon className="w-16 h-16 text-blue-500" />
+      <Icon className="w-16 h-16 text-black" />
 
       {/* Text */}
       <p className="text-gray-800 text-lg font-medium mt-4">{text}</p>
